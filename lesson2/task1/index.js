43c894f74f1ce5f27d4e63279808ab094456bed2
sosibona <empty.tara@gmail.com>
2020-01-23 22:40:30 +0200

    variables1 fix

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -3,6 +3,6 @@ const greeting = "I'm John";
 const _profession = 'Developer';
 const $hobby = 'Walking';
 const favouriteDessert = 'cake';
-let pet = 'dog';
+let pet_ = 'dog';
 let superNumber;
 superNumber = 17;
