d0917ff8c8801004f70d65eb7c01223a7d484025
sosibona <empty.tara@gmail.com>
2020-01-26 00:23:40 +0200

    task3_1

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -24,7 +24,7 @@ console.log('17 === 1: ', 17 === 1);
 console.log('17 === 17: ', 17 === 17);
 console.log('17 === \'17\': ', 17 === '17');
 console.log('17 === true: ', 17 === true);
-console.log('0 === \'\': ', 17 === '');
+console.log('0 === \'\': ', 0 === '');
 
 console.log('true === false: ', true === false);
 console.log('true === true: ', true === true);
@@ -45,9 +45,9 @@ console.log('false === 0: ', false === 0);
 /* нестрогое сравнение */
 console.log('17 == \'17\': ', 17 == '17');
 // 17 == '17';
-console.log('\'0\' == \'\': ', 17 == '');
+console.log('\'0\' == \'\': ', '0' == '');
 // '0' == '';
-console.log('0 == \'\': ', 17 == '');
+console.log('0 == \'\': ', 0 == '');
 // 0 == '';
 // null == undefined;
 // false == 0;
