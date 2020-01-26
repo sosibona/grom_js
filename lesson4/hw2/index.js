ae6fa36ef3a71d6d114bbc66b9e1031921e299d0
sosibona <empty.tara@gmail.com>
2020-01-26 21:40:55 +0200

    fix3

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -3,9 +3,11 @@ const n = 13;
 let result = 0;
 
 for (let i = m; i <= n; i++) {
-  if ((i % 2 === 0) && (i % 4 !== 0)) result += i;
-  if (i % 3 === 0) result -= i;
-  if (i % 4 === 0) result *= i;
+  if ((i % 2 === 0) && (i % 4 !== 0)) {
+    result += i;
+  } else if (i % 3 === 0) {
+    result -= i;
+  } else if (i % 4 === 0) result *= i;
   if (i % 5 === 0) console.log(i);
 }
 // console.log(result);
