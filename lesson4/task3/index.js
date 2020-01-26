f909bf12a87135db36f692a31d6d892d2ce27605
sosibona <empty.tara@gmail.com>
2020-01-26 21:05:39 +0200

    task

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 5; i++) {
-  for (let y = 1; y <= 5; y++) {
+  for (let y = 1; y <= 10; y++) {
     console.log(i + ' x ' +  y + ' = ' + i * y);
   }
   console.log('=> ' + i + ' <=');
