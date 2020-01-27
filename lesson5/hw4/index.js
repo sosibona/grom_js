108bb42906ddaa0c86cfa2146335ba1777ea0be4
sosibona <empty.tara@gmail.com>
2020-01-27 15:00:58 +0200

    solution

---
+++ lesson5/hw4/index.js
@@ -0,0 +1,13 @@
+function compareSum(a, b, c, d) {
+  return sum(a, b) > sum(c, d) ? true : false;
+}
+
+function sum(from, to) {
+  let sum = 0;
+  for (let i = from; i < to; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(compareSum(1, 6, 2, 4));
