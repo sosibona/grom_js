29abd615e16511f7f0a9364b2cd8422bdb518133
sosibona <empty.tara@gmail.com>
2020-01-28 01:22:50 +0200

    solution

---
+++ lesson6/hw4/index.js
@@ -0,0 +1,9 @@
+function uniqueCount(array) {
+  const uniqueAray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (uniqueAray.indexOf(array[i]) === -1) uniqueAray.push(array[i]);
+  }
+  return uniqueAray;
+}
+
+console.log(uniqueCount([1, 2, 4, 1, 3,4 ,5, 5]));
