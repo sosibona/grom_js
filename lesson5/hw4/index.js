6348d85a2fa8ae69761151ab0d22090097ff7373
sosibona <empty.tara@gmail.com>
2020-01-27 15:02:46 +0200

    fix

--- lesson5/hw4/index.js
+++ lesson5/hw4/index.js
@@ -4,7 +4,7 @@ function compareSum(a, b, c, d) {
 
 function sum(from, to) {
   let sum = 0;
-  for (let i = from; i < to; i++) {
+  for (let i = from; i <= to; i++) {
     sum += i;
   }
   return sum;
