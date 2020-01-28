8e444354ade76797e738f18be3e0a1e04f174bb4
sosibona <empty.tara@gmail.com>
2020-01-28 16:50:51 +0200

    solution

--- lesson6/hw6/index.js
+++ lesson6/hw6/index.js
@@ -1,6 +1,7 @@
-function checker(arr) {
-  if (!Array.isArray(arr)) return null;
-  const maxElement = Math.max(...arr);
-  const minElement = Math.min(...arr);
-  return maxElement + minElement > 1000 ? true : false;
+function withdraw(clients, balances, client, amount) {
+  const indexOfClient = clients.indexOf(client);
+  const operationMoney = balances[indexOfClient] - amount
+  return operationMoney >= 0 ? operationMoney : -1;
 }
+
+// console.table(withdraw([1, 2, 3, 4], [100, 100, 100, 100], 3, 55));
