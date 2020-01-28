a6db6585aace53e893f09bbccac880445bc2ac33
sosibona <empty.tara@gmail.com>
2020-01-28 14:41:42 +0200

    fix

--- lesson6/hw6/index.js
+++ lesson6/hw6/index.js
@@ -2,5 +2,5 @@ function checker(arr) {
   if (Array.isArray(arr)) return null;
   const maxElement = Math.max(...arr);
   const minElement = Math.min(...arr);
-  return maxElement + minElement < 1000 ? true : false;
+  return maxElement + minElement >= 1000 ? true : false;
 }
