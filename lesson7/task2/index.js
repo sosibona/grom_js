9ebbfc4d9f4373fb1c9e8fb0c348ba6ca6654f49
sosibona <empty.tara@gmail.com>
2020-01-28 22:17:50 +0200

    fix

--- lesson7/task2/index.js
+++ lesson7/task2/index.js
@@ -1,9 +1,8 @@
 function sortDesc(array) {
-  const sortArray = array.sort((a, b) => {
+  const copyArray = array.slice();
+  return copyArray.sort((a, b) => {
     return b - a;
   });
-
-  return sortArray;
 }
 
 console.log(sortDesc([1, 5, 3, 7, -10]));
