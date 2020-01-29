beb5ebe94240851c560f36cffcf73d20a254ac3f
sosibona <empty.tara@gmail.com>
2020-01-30 00:39:16 +0200

    solution2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,5 +1,12 @@
 function getRandomNumbers(length, start, end) {
-  const res = []
+  if (end < start || length < 1 || end - start < 2) return null;
+  if (end - start === 2) {
+    const res = new Array(2);
+    return res.fill((end + start) / 2);
+  }
+
+  const res = [];
+
   while (res.length !== 5) {
     let randomNumber = Math.trunc(end * Math.random());
     if (randomNumber > start) res.push(randomNumber);
@@ -8,4 +15,4 @@ function getRandomNumbers(length, start, end) {
   return res;
 }
 
-console.log(getRandomNumbers(5, 1, 10));
+console.log(getRandomNumbers(5, 8, 10));
