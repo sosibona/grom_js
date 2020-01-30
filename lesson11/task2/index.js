ba28d7be28fd1f9919068610bf4c7ba9a0c5134e
sosibona <empty.tara@gmail.com>
2020-01-30 12:53:19 +0200

    solution

---
+++ lesson11/task2/index.js
@@ -0,0 +1,35 @@
+function sortContacts(contacts, sorting = true) {
+  if (!Array.isArray(contacts)) return null;
+
+  if (sorting) {
+    return contacts.sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    });
+  } else {
+   return contacts.sort((a, b) => {
+      return b.name.localeCompare(a.name)
+    });
+  }
+
+}
+
+contactList = [
+  {
+    name: 'Taras',
+    phoneNumber: '1'
+  },
+  {
+    name: 'Tom',
+    phoneNumber: '2'
+  },
+  {
+    name: 'Ann',
+    phoneNumber: '4'
+  },
+  {
+    name: 'Bob',
+    phoneNumber: '1'
+  },
+];
+
+console.log(sortContacts(contactList));
