868ebe4f1f9359d6a0e5cf4f6c16f1b8a5a30160
sosibona <empty.tara@gmail.com>
2020-01-30 14:59:49 +0200

    solution

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -1,5 +1,6 @@
 function countOccurrences(string, subString) {
+  if (subString.length === 0) return null;
   return string.split(subString).length - 1;
 }
 
-console.log(countOccurrences('tmatma mama', 'ma'));
+console.log(countOccurrences('tmatma mama', ''));
