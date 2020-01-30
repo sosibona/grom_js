f99f042e86c715a14a78a23245ea0aa9d9f09de2
sosibona <empty.tara@gmail.com>
2020-01-30 14:35:41 +0200

    solution2

---
+++ lesson11/hw1/index.js
@@ -0,0 +1,24 @@
+function splitString(str, number) {
+  const arrStr = [];
+  let startPosition = 0;
+
+  if (typeof str !== 'string') return null;
+
+  while(true) {
+    let piece = str.substr(startPosition, number);
+    if (piece.length === 0) break;
+
+    startPosition += number;
+    arrStr.push(piece);
+  }
+  
+  let lastElement = arrStr[arrStr.length - 1];
+
+  lastElement += '.'.repeat(number - lastElement.length);
+  arrStr[arrStr.length - 1] = lastElement;
+  
+  return arrStr;
+
+}
+
+console.log(splitString('asdasadasd', 3));
