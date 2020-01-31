c716e5e85e3a77a313b1a84a33fb01c2173850c8
sosibona <empty.tara@gmail.com>
2020-01-31 14:38:18 +0200

    solution

---
+++ lesson14/task2/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let message = 'Just learn it';
+
+function createMessanger() {
+  function sentMessage(name) {
+    const sender = 'Gromcode';
+
+    console.log(`${name}, ${message}! Your ${sender}`);
+  }
+
+  function setMessage(text) {
+    message = text;
+  }
+
+  function setSender(text) {
+    message  = text;
+  ;
+  }
+
+  return {
+    sentMessage,
+    setMessage,
+    setSender,
+  }
+}
+
+const mes1 = createMessanger();
+const mes2 = createMessanger();
+
+mes1.sentMessage('Taras');
+mes2.sentMessage('Olesya');
+mes1.setMessage('Good job');
+mes2.setMessage('Good job');
+mes1.setSender('Just learn it');
+mes1.sentMessage('Taras');
+mes2.sentMessage('Olesya');
