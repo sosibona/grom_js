ca834cd22febcf27b855d616e557d638b68abe5a
sosibona <empty.tara@gmail.com>
2020-02-04 09:59:47 +0200

    solution

---
+++ lesson17/task3/index.js
@@ -0,0 +1,22 @@
+function defer(func, ms) {
+  return function() {
+    setTimeout(() => {func.call(this,...arguments)}, ms);
+  }
+}
+
+// const sayHi = () => {
+//   console.log('Hi');
+// }
+
+const user = {
+  name: 'Taras',
+  sayHi(){
+    console.log(`Hi, my name is ${this.name}`);
+  }
+}
+
+// sayHi();
+
+const deferDone = defer(user.sayHi, 1500);
+
+deferDone.call({name: 'Ann'});
