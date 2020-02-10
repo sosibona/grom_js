a70d16c66332f6de3d89cc989c04cad9cca5b5cd
sosibona <empty.tara@gmail.com>
2020-02-10 22:18:49 +0200

    solution

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,13 +1,13 @@
 export const event = {
-  message: 'Welcome to the party!',
+  message: 'Welcome to the party',
   guests: [
     {name: "John", age: 21, email: "example@gmail.com"},
     {name: "Mat", age: 18, email: "mat@gmail.com"},
     {name: "Bob", age: 10, email: "bob@gmail.com"}
   ],
   getInvintations(){
-    return this.guests.filter(({age}) => age > 18).map(({email, name}) => ({
-      text: `Dear ${name}! ${this.message}`,
+    return this.guests.filter(({age}) => age > 18).map(({name, email}) => ({
+      text: `Dear ${name}! ${this.message}!`,
       email
     }));
   }
