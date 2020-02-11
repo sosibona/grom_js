b86ba1802c04b4af0ccb685a01da3ad82163510d
sosibona <empty.tara@gmail.com>
2020-02-11 12:13:24 +0200

    solution

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,3 +1,20 @@
 export function getOwnProps(obj){
-  return Object.keys(obj);
+  let arr = []
+  // return Object.keys(obj).filter(el => typeof el !== 'function');
+  for (let prop in obj) {
+    if (typeof obj[prop] !== 'function') {
+    arr.push(prop);
+    }
+  return arr;
+  }
 }
+
+const obj = {
+  name: 'Test',
+  sayHi(){
+    console.log('Hi');
+  }
+}
+
+console.log(getOwnProps(obj));
+
