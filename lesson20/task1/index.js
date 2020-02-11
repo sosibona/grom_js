d2e3bd13d06a69eff90340bae87890af92ce00c5
sosibona <empty.tara@gmail.com>
2020-02-11 15:57:38 +0200

    solution

---
+++ lesson20/task1/index.js
@@ -0,0 +1,22 @@
+export class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+  }
+
+  requestNewPhoto(){
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+
+  setAge(newAge){
+    if (age < 0) return false;
+
+    this.age = newAge;
+    if (age >= 25) this.requestNewPhoto();
+    return this.age;
+  }
+}
