765b119c1e45a1f8a0881b4b93ccac61e61049d4
sosibona <empty.tara@gmail.com>
2020-02-11 22:22:01 +0200

    solution

---
+++ lesson20/hw1/index.js
@@ -0,0 +1,29 @@
+class Order {
+  constructor(price, city, type){
+    this.price = price;
+    this.city = city;
+    this.type = type;
+    this.dateCreated = new Date();
+    this.isConfirmed = false;
+    this.id = Math.random().toString(36).substring(7);
+  }
+
+  checkPrice(){
+    return this.price > 1000 ? true : false;
+  }
+
+  confirmOrder(){
+    if (this.isConfirmed) return;
+    this.isConfirmed = true;
+    this.dateCreated = new Date();
+    return;
+  }
+
+  isValidType(){
+    if (this.type === 'Buy' || this.type === 'Sell') return true;
+    return false;
+  }
+}
+
+const order1 = new Order(1000, 'Lviv', 'Sell');
+console.log(order1);
