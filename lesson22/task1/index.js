dc9d5a11e535d0e884a41bdbab05dd2edb3983ec
sosibona <empty.tara@gmail.com>
2020-02-15 23:43:28 +0200

    solution

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -2,17 +2,44 @@ const elemDiv = document.querySelector('.rect_div');
 const elemP = document.querySelector('.rect_p');
 const elemSpan = document.querySelector('.rect_span');
 
-// const logTarget = (text, color) => {
-//   const eventListElem = document.querySelector('.vents-list');
+const btnClear = document.querySelector('.btn-clear');
+const btnRemoveHandleds = document.querySelector('.btn-remove');
+const btnAttatchHandleds = document.querySelector('.btn-attach');
 
-//   eventListElem.innerHTML += `<span style="color: ${color}">${text}</span>`;
-// }
 
 function logTarget (text, color){
   const eventListElem = document.querySelector('.events-list');
 
   eventListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
 }
+
+function clearLog(){
+  const eventListElem = document.querySelector('.events-list');
+
+  eventListElem.innerHTML = '';
+}
+
+function removeHandleds(){
+  elemDiv.removeEventListener('click', logGreyDiv, true);
+  elemP.removeEventListener('click', logGreyP, true);
+  elemSpan.removeEventListener('click', logGreySpan, true);
+  
+  elemDiv.removeEventListener('click', logGreenDiv);
+  elemP.removeEventListener('click', logGreenP);
+  elemSpan.removeEventListener('click', logGreenSpan);
+}
+
+function attatchHandleds(){
+  elemDiv.addEventListener('click', logGreyDiv, true);
+  elemP.addEventListener('click', logGreyP, true);
+  elemSpan.addEventListener('click', logGreySpan, true);
+  
+  
+  elemDiv.addEventListener('click', logGreenDiv);
+  elemP.addEventListener('click', logGreenP);
+  elemSpan.addEventListener('click', logGreenSpan);
+}
+
 
 const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
 const logGreenP = logTarget.bind(null, 'P', 'green');
@@ -32,3 +59,9 @@ elemSpan.addEventListener('click', logGreySpan, true);
 elemDiv.addEventListener('click', logGreenDiv);
 elemP.addEventListener('click', logGreenP);
 elemSpan.addEventListener('click', logGreenSpan);
+
+btnClear.addEventListener('click', clearLog);
+btnRemoveHandleds.addEventListener('click', removeHandleds);
+btnAttatchHandleds.addEventListener('click', attatchHandleds);
+
+
