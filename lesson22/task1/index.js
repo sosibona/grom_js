ff8e30d2ab3395671d284c5e833a4c391505825f
sosibona <empty.tara@gmail.com>
2020-02-16 00:02:28 +0200

    fix

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -43,11 +43,11 @@ function attatchHandleds(){
 
 const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
 const logGreenP = logTarget.bind(null, 'P', 'green');
-const logGreenSpan = logTarget.bind(null, 'Span', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
 
 const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
-const logGreySpan = logTarget.bind(null, 'Span', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
 
 
