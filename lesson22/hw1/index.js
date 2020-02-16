208e08d666b0f758f6c99883064be6145c822d02
sosibona <empty.tara@gmail.com>
2020-02-17 01:32:46 +0200

    create add new task

--- lesson22/hw1/index.js
+++ lesson22/hw1/index.js
@@ -1,11 +1,31 @@
+const btnCreateTask = document.querySelector('.todo__button');
+
 const task = [
-    { text: 'Buy milk', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: false },
-    { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
-  ];
-  
+    { text: 'Buy milk', dateCreate: new Date(), done: false },
+    { text: 'Pick up Tom from airport', dateCreate: new Date(), done: false },
+    { text: 'Visit party', dateCreate: new Date(), done: false },
+    { text: 'Visit doctor', dateCreate: new Date(), done: true },
+    { text: 'Buy meat', dateCreate: new Date(), done: true },
+  ];    
+
+function createNewTask(){
+    let getText = document.querySelector('.task-input').value;
+    if (getText.length === 0) return;
+    task.push({
+        text: getText,
+        dateCreate: new Date(),
+        done: false,
+      });
+      document.querySelector('.task-input').value = '';
+      const listElem = document.querySelector('.list');
+      listElem.innerHTML = '';
+      renderListItems(task);    
+}
+
+  btnCreateTask.addEventListener('click', createNewTask);
+
+
+
   const renderListItems = listItems => {
     const  listElem = document.querySelector('.list');
   
@@ -17,20 +37,32 @@ const task = [
       checkboxElem.setAttribute('type', 'checkbox');
       listItemElem.classList.add('list__item'); 
       if (done) listItemElem.classList.add('list__item_checked');
-      checkboxElem.checked = done;
+        checkboxElem.checked = done;
       listItemElem.append(checkboxElem, text);
-  
       return listItemElem;
     });
+
+    console.log(listTasks);
     
-  
     listElem.append(...listTasks);
   
-  
     return listTasks;
-    
-    
   };
   
-  
   renderListItems(task);
+
+
+//   const checkboxElem = document.querySelector('.list__checkbox');
+
+//   checkboxElem.addEventListener('click', doneTask);
+
+//   function doneTask(){
+//       const listItemElem = document.querySelector('.list__item');
+
+//       listItemElem.classList.toggle('list__item_checked')
+
+//       checkboxElem.setAttribute.toggle('checked', 'checked');
+//   }
+
+  
+  
