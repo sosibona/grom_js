db71c9383f243d6aecadaea40cbf88a70f5b29bd
sosibona <empty.tara@gmail.com>
2020-02-19 00:56:03 +0200

    solution

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -1,50 +1,35 @@
 const monthOfYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const students = [
-  {name: 'Tom', birthDay: '01/15/2010'},
-  {name: 'Ann', birthDay: '01/02/2002'},
-  {name: 'Bob', birthDay: '01/05/2000'},
+  {name: 'Tom', birthDay: '11/15/2010'},
+  {name: 'Ann', birthDay: '11/02/2002'},
+  {name: 'Bob', birthDay: '11/05/2000'},
   {name: 'Rob', birthDay: '01/10/2011'},
-  {name: 'Karl', birthDay: '03/23/1999'},
-  {name: 'Yan', birthDay: '03/30/1987'},
-  {name: 'Zak', birthDay: '03/03/1988'},
-  {name: 'Jerry', birthDay: '03/06/2012'},
-  {name: 'John', birthDay: '01/06/2013'},
+  {name: 'Karl', birthDay: '08/23/1999'},
+  {name: 'Yan', birthDay: '08/30/1987'},
+  {name: 'Zak', birthDay: '08/03/1988'},
+  {name: 'Jerry', birthDay: '12/06/2012'},
+  {name: 'John', birthDay: '01/05/2013'},
 ];
 
 export function studentsBirthDays(students){
 
   const month = {};
-  const s1 = [];
   for (let i = 0; i < students.length; i++) {
     const studentsCopy = Object.assign({}, students[i]);
-    s1.push(studentsCopy);
-  }
-  const s3 = s1
-      .map(elem => ({
-        name: elem.name,
-        birthDay: elem.birthDay,
-        month: new Date(elem.birthDay).getMonth(),
-      })).sort((a, b) => a.month - b.month).map(elem => ({
-        name: elem.name,
-        birthDay: elem.birthDay,
-      }));
-  
-  for (let i = 0; i < s3.length; i++) {
-  
-    let checkMonth = monthOfYear[new Date(s3[i].birthDay).getMonth()];
+   
+    let checkMonth = monthOfYear[new Date(studentsCopy.birthDay).getMonth()];
     if (!month.hasOwnProperty(checkMonth)) {
       month[checkMonth] = [];
-      month[checkMonth].push(s3[i])
+      month[checkMonth].push(studentsCopy)
     } else {
-      month[checkMonth].push(s3[i]);
+      month[checkMonth].push(studentsCopy);
     }
   }
 
   for (let students in month) {
     month[students] = month[students]
-        .map(objName => ({name: objName.name, birthDay: objName.birthDay = new Date(objName.birthDay).getDate()}))
-        .sort((a, b) => a.birthDay - b.birthDay)  
+        .sort((a, b) => new Date(a.birthDay) - new Date(b.birthDay))
         .map(elem => elem.name);
   }
 
