b25c34101b8384760046708c945c2b9ea3aab207
sosibona <empty.tara@gmail.com>
2020-03-04 23:45:18 +0200

    callback

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -8,17 +8,24 @@ const addImage = (imgSrc, callback) => {
 
   const onImageLoaded = () => {
     const { width, height } = imgElem; 
-    callback({width, height});
+    callback(null, {width, height});
     
   }
 
   imgElem.addEventListener('load', onImageLoaded);
+
+  imgElem.addEventListener('error', () => callback('Image load failed'));
 }
 
 const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
 
 
-const onImageLoaded = ({ width, height }) => {
+const onImageLoaded = (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;  
 };
