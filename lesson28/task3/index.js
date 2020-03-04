aa692a15b6f1ad8bbb6c59cf2798baeff314915a
sosibona <empty.tara@gmail.com>
2020-03-04 10:16:34 +0200

    fix

--- lesson28/task3/index.js
+++ lesson28/task3/index.js
@@ -1,8 +1,9 @@
 export const mult = a => b => a * b;
 
-export const twice = mult(7);
+export const twice = mult(7)(2);
 
-export const triple = twice(9);
+export const triple = mult(9)(3);
 
 console.log(triple);
+console.log(twice);
 console.log(mult(5)(4));
