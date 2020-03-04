f914032abd6ebf4aa52f7bdca3e4f2857e01bb8c
sosibona <empty.tara@gmail.com>
2020-03-04 12:31:46 +0200

    fibonacci

---
+++ lesson28/hw2/index.js
@@ -0,0 +1,18 @@
+export const maxFibonacci = (value) => {
+
+  const fibobacci = [0, 1]
+  let i = 2;
+
+  while (true) {
+    fibobacci[i] = fibobacci[i - 1] + fibobacci[i - 2];
+
+    fibobacci.push(fibobacci[i])
+    
+    if (fibobacci[i] > value) return fibobacci[i - 1];
+    i++;
+  }
+}
+
+console.log(maxFibonacci(15));
+
+
