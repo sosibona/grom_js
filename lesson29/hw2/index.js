ce225c9aca2f0e6424802beee49d87ff2bb9f19c
sosibona <empty.tara@gmail.com>
2020-03-05 10:45:22 +0200

    solution

--- lesson29/hw2/index.js
+++ lesson29/hw2/index.js
@@ -1,7 +1,5 @@
-export function delay(delay, callback, context, ...arguments) {
+export function delay(delay, callback, context, arguments) {
 
-  return function() {
-    setTimeout(() => callback.apply(this, [arguments]), delay);
-  };
+      setTimeout(() => callback.apply(context, [...arguments]), delay);
 
 }
