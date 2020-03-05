d8f0f87eda6ee6115e5b78522f2a00986f5c2b0d
sosibona <empty.tara@gmail.com>
2020-03-05 12:20:26 +0200

    solution

---
+++ lesson30/task1/index.js
@@ -0,0 +1,45 @@
+const addImageWithPromise = imgSrc => {
+  const p = new Promise((resolveCb, rejectCb) => {
+
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem; 
+      resolveCb({width, height});
+      
+    }
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')));
+  });
+
+  return p;
+};
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amaznaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+
+const resultPromise = addImageWithPromise(imgSrc);
+
+resultPromise.catch(error => console.log(error));
+
+resultPromise.then(data => console.log(data));
+
+// console.log(result);
+
+
+// const onImageLoaded = (error, data) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   const { width, height } = data;
+//   const sizeElem = document.querySelector('.image-size');
+//   sizeElem.textContent = `${width} x ${height}`;  
+// };
+
+// addImage(imgSrc, onImageLoaded);
