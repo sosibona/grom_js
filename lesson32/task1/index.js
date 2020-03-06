d04c40d41b972fe5ea098aedc2b25bc4586d554c
sosibona <empty.tara@gmail.com>
2020-03-06 16:06:06 +0200

    sol

--- lesson32/task1/index.js
+++ lesson32/task1/index.js
@@ -1,3 +1,14 @@
+const getValueWithDelay = (value, delay) => new Promise(resolve => {
+  setTimeout(() => {
+    console.log(value);
+    resolve(value);
+  }, delay);
+});
+
+const asyncNumbers1 = getValueWithDelay(56, 1000);
+const asyncNumbers2 = getValueWithDelay(undefined, 2000);
+const asyncNumbers3 = getValueWithDelay('10', 3000);
+
 const getSum = numbers => {
   return numbers
     .filter(numbers => !isNaN(numbers))
@@ -11,4 +22,5 @@ export const asyncSum = (...asyncNumbers) => {
 }
 
 asyncSum(asyncNumbers1, asyncNumbers2, asyncNumbers3)
+  
   .then(result => console.log(result));
