15e822374f532d7bc5ff4fb4093fc20c1598d654
sosibona <empty.tara@gmail.com>
2020-03-17 15:44:43 +0200

    Solution

---
+++ lesson35/task2/index.js
@@ -0,0 +1,50 @@
+const successRequest = Promise.resolve({ name: "Tom" });
+
+successRequest
+    .then(function onSuccess1(data) {
+        console.log(data);
+        throw new Error('Error with data');
+    })
+    .catch(function onError1(error) {
+        console.error("onError1", error.message);
+    })
+
+
+const failRequest = Promise.reject(new Error("Something went wrong"));
+
+failRequest
+    .catch(function onError2(error) {
+        console.error("onError2", error.message);
+        throw new Error('Server error')
+    })
+    .then(function onSuccess2(data) {
+        console.log("onSuccess2", data);
+    })
+    .catch(function onError3(error) {
+        console.error("onError3", error.message);
+    });
+
+// const successRequest = Promise.resolve({name: 'Taras'});
+
+// successRequest
+//   .then(data => {
+//     console.log(data);
+//     throw new Error('Unexpected error')
+//   })
+//   .catch(err => {
+//     console.error(err);
+    
+//   })
+
+// const failedRequest = Promse.reject(new Error('Fail'));
+
+// failedRequest
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.error(err.message);
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
