be4deb3c4be80f85e0c24525de34f5172b97473c
sosibona <empty.tara@gmail.com>
2020-03-17 15:54:44 +0200

    Solution

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -2,7 +2,6 @@ const successRequest = Promise.resolve({ name: "Tom" });
 
 successRequest
     .then(function onSuccess1(data) {
-        console.log(data);
         throw new Error('Error with data');
     })
     .catch(function onError1(error) {
@@ -14,7 +13,6 @@ const failRequest = Promise.reject(new Error("Something went wrong"));
 
 failRequest
     .catch(function onError2(error) {
-        console.error("onError2", error.message);
         throw new Error('Server error')
     })
     .then(function onSuccess2(data) {
