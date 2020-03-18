1356525f68d50fcffe83c8aca7af8caa9576d16e
sosibona <empty.tara@gmail.com>
2020-03-18 15:22:32 +0200

    sol

---
+++ lesson36/task1/index.js
@@ -0,0 +1,16 @@
+export const fetchUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+  const userData = await response.json();
+  console.log(userData);
+  } catch (err) {
+  throw new Error('Failed to fetch user');
+  }
+}
+
+getUser('sosibona')
+  then(userData => console.log(userData))
+  .catch(err => alert(err.message));
