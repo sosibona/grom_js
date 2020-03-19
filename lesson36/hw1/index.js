ccb537c34b6ea98bbe8e730dde5503f4c5c799f3
sosibona <empty.tara@gmail.com>
2020-03-19 15:26:02 +0200

    solution

--- lesson36/hw1/index.js
+++ lesson36/hw1/index.js
@@ -1,57 +1,16 @@
-// const getUserData = (user) => {
+export const getUsersBlogs = async usersList => {
+  const requests = usersList
+      .map(userId => fetch(`https://api.github.com/users/${userId}`)
+          .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              throw new Error('Error');
+          }));
+  const usersData = await Promise.all(requests);
+  return usersData.map(user => user.blog);
+};
 
-//   const url = `https://api.github.com/users/${user}`
-//   const response = fetch(url)
-//       .then(response => {
-//         if (response.ok) {
-//           return response.json();
-//         }
-//         throw new Error('Error');
-//       })
-//       .then(value => value.blog)
-//       .catch(err => err);
+const users = ['google', 'facebook', 'sosibona'];
 
-//   return response;
-// }
-
-// export const getUsersBlogs = async userList => {
-//   try {
-//       const promiseList = userList.map(user => getUserData(user));
-//       const response = await Promise.all(promiseList)
-//       return response;
-//   }
-//   catch (err) {
-//       return console.log(err.message)
-//   }
-// }
-
-// getUsersBlogs(['facebook', 'github', 'sosibona'])
-const getUserData = (userId) => {
-
-  const baseLink = `https://api.github.com/users/${userId}`
-  const response = fetch(baseLink)
-      .then(response => {
-          if (response.ok) {
-              return response.json();
-          } throw new Error('Loading failed');
-      })
-      .then(value => value.blog)
-      .catch(err => err);
-
-  return response
-}
-
-export const getUsersBlogs = async (userIdArr) => {
-  try {
-      const arrOfPromises = userIdArr.map(item => getUserData(item));
-
-      const response = await Promise.all(arrOfPromises)
-      console.log(response)
-      return response;
-  }
-  catch (err) {
-      return console.log(err.message)
-  }
-}
-
-getUsersBlogs(['facebook', 'github', 'steve'])
+getUsersBlogs(users);
