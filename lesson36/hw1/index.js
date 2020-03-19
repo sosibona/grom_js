ce1d7af3eef45a5eb9bee882fe8126693fdba703
sosibona <empty.tara@gmail.com>
2020-03-19 15:12:14 +0200

    solution

---
+++ lesson36/hw1/index.js
@@ -0,0 +1,25 @@
+const getUserData = (user) => {
+
+  const url = `https://api.github.com/users/${user}`
+  const response = fetch(url)
+      .then(response => response.json())
+      .then(value => value.blog)
+      .catch(err => alert(err.message));
+
+  return response;
+}
+
+const getUsersBlogs = async userList => {
+  try {
+      const promiseList = userList.map(user => getUserData(user));
+
+      const response = await Promise.all(promiseList)
+      console.log(response)
+      return response;
+  }
+  catch (err) {
+      return console.log(err.message)
+  }
+}
+
+getUsersBlogs(['facebook', 'github', 'sosibona'])
