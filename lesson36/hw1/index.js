cddcda84158505c0094b1e7078307d3582a0a832
sosibona <empty.tara@gmail.com>
2020-03-19 15:36:37 +0200

    solution

--- lesson36/hw1/index.js
+++ lesson36/hw1/index.js
@@ -1,16 +1,24 @@
+const fetchUser = user => {
+  return fetch(`https://api.github.com/users/${user}`)
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Error');
+    })
+    .then(function(userData){
+      return userData.blog;
+    })
+}
+
 export const getUsersBlogs = async usersList => {
-  const requests = usersList
-      .map(userId => fetch(`https://api.github.com/users/${userId}`)
-          .then(response => {
-              if (response.ok) {
-                return response.json();
-              }
-              throw new Error('Error');
-          }));
-  const usersData = await Promise.all(requests);
-  return usersData.map(user => user.blog);
+  const allPromise = usersList.map(user => fetchUser(user))
+  const usersData = await Promise.all(allPromise);
+  console.log(usersData);
+  return usersData;
 };
 
 const users = ['google', 'facebook', 'sosibona'];
 
 getUsersBlogs(users);
+
